45586790fa30b7a06c1faa809afa5f256b6c9f68
onlyyou112 <1292861831@qq.com>
2019-07-06 22:31:52 +0800

    加入定时清理缓存，每两个小时清理一次缓存；

--- src/main/java/com/xhy/xhyappservier/XhyappservierApplication.java
+++ src/main/java/com/xhy/xhyappservier/XhyappservierApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableScheduling
 @EnableCaching
 public class XhyappservierApplication extends SpringBootServletInitializer {
 
---
+++ src/main/java/com/xhy/xhyappservier/service/CleanService.java
@@ -0,0 +1,14 @@
+package com.xhy.xhyappservier.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: xhyappservier
+ * @description: 清理缓存服务
+ * @author: Mr.Wang
+ * @create: 2019-07-06 22:01
+ **/
+
+public interface CleanService {
+    public void clean();
+}
